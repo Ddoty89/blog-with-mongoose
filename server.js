489537adbfb9e8ac065c8387804739ed3d6e8573
Doty's <dotys@Dotyss-iMac.local>
2017-11-16 19:58:45 -0600

    server

--- server.js
+++ server.js
@@ -13,17 +13,31 @@ app.get('/posts', function(req, res) {
 })
 
 app.get('/posts/:id', function(req, res) {
-	blogPost.findById()
+	blogPost.findById(req.params.id)
 })
 
 app.post('/posts', function(req, res) {
-	blogPost.
+	const requriedFields = ['title', 'content', 'author'];
+	for(let i = 0; i < requriedFields; i++) {
+		if (!(requiredFields in req.body)) {
+    	const message = `Missing \`${field}\` in request body`
+    	console.error(message);
+    	return res.status(400).send(message);
+    }
+	}
+
+	blogPost.create({
+      "title": req.body.title,
+      "content": req.body.content,
+      "author": req.body.author
+      }
+	})
 })
 
 app.put('/posts/:id', function(req, res) {
-
+	blogPost.findById(req.params.id)
 })
 
 app.delete('/posts/:id', function(req, res) {
-	
+	blogPosts.findByIdAndRemove(req.params.id)
 })
